8e464856ff43af6640b3c9d36a3b4191a34ad853
Jason Hamm <jhamm.business@gmail.com>
2012-02-06 06:10:35 -0500

    Initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jamba::Application.config.secret_token = 'd407164ff4f842613d12040e30035281efb0576961138f5d63d1f51169dfc581f9306f957a365baffe8428268f15ba93d3c6fb879c449fb63bd23888490b5303'
